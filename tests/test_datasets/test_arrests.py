abc2ff31f96280921046a1b99ba1d8d5ce6cc576
Vincent <vincentwarmerdam@gmail.com>
2020-04-18 15:38:09 +0200

    added-fairness-warning

--- tests/test_datasets/test_arrests.py
+++ tests/test_datasets/test_arrests.py
@@ -1,5 +1,13 @@
+import pytest
+
 from skfair.datasets import load_arrests
+from skfair.warning import FairnessWarning
 
 
 def test_shape_arrests():
     assert load_arrests()['data'].shape == (5226, 7)
+
+
+def test_raise_warning():
+    with pytest.warns(FairnessWarning):
+        load_arrests()
